4f433a549adcb0e487527190b53db635ed3663f2
Shamithasivaa <128037182+Shamithasivaa@users.noreply.github.com>
2023-04-05 15:26:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Web from './Web';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Webfooter from './Webfooter';
+
+function App() {
+  return (
+    <div className="App">
+      <Web/>
+      <Webfooter/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
